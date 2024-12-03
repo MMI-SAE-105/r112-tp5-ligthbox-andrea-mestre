df8c17a8429dc87c702df6dd54e825ae31cbad31
andrea-mestre <andrea.mestre@edu.univ-fcomte.fr>
2024-12-03 16:47:16 +0100

    js

--- src/js/script.js
+++ src/js/script.js
@@ -4,5 +4,8 @@ const imageAnimaux = document.querySelectorAll('#lightbox > img');
 
 
 document.addEventListener('click', (evt) {
-    
-}
+  if(evt.target.matches(img[data-full-img])) {
+    const fullImgUrl = evt.target.data-full-img;
+    lightboxAnimaux.src = fullImgUrl;
+    lightbox.showModal();
+}});
